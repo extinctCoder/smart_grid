36495a9b7bad0972d0d97114e981c0987151e78f
Sabbir Ahmed Shourov <name@example.com>
2025-08-02 12:55:16 +0600

    docs: add instructions for creating MQTT password file
    Add detailed documentation on creating a Mosquitto MQTT password file
    using a temporary Docker container running mosquitto_passwd. This
    improves security by guiding users to generate encrypted credentials
    for the MQTT broker user. Remove the deprecated helper script to avoid
    redundancy and encourage using the documented Docker command instead.

--- helpers.sh
+++
@@ -1,4 +0,0 @@
-# Create a password file for the MQTT broker user 'extinctcoder'
-# This runs mosquitto_passwd in a Docker container to generate an encrypted password
-# The password file will be created at docker/mosquitto/config/password.txt
-docker run -it --rm -v "$PWD/docker/mosquitto/config:/mosquitto/config" eclipse-mosquitto mosquitto_passwd -c /mosquitto/config/password.txt extinctcoder
